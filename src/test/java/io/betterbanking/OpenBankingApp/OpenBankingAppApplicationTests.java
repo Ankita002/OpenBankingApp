3c6d80d4456eacb20e87bd0b8cad224b5e8e72a9
Ankita002 <ankitarafiz@gmail.com>
2022-06-29 04:16:33 +0530

    initialized project

---
+++ src/test/java/io/betterbanking/OpenBankingApp/OpenBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.betterbanking.OpenBankingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenBankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
